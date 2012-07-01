d0c4dc4484169f0153efa0e86a875f4e64d7453b
chadsorensen <chdsorensen@gmail.com>
2012-07-01 10:48:48 -0700

    initial baby log commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyLog::Application.config.secret_token = 'd41b928c87eab23e0ffa42c9a31e8216bdc2639d23ad9f4b790d8c1748b66db706d818a2032764595c33dacc9daaa99b872beefbc130202d42541b2c527a3717'
